e130fd88317ddbdad778288b8be5d234985ce048
Heiky <hongtaoguo0214@sina.com>
2020-07-10 16:38:09 +0800

    update IPUtil

--- call-statistics-jdbc/src/main/java/org/kaws/util/IPUtil.java
+++ call-statistics-jdbc/src/main/java/org/kaws/util/IPUtil.java
@@ -35,7 +35,8 @@ public class IPUtil {
                 }
             }
             // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-            if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
+            // "***.***.***.***".length()
+            if (ipAddress != null && ipAddress.length() > 15) {
                 // = 15
                 if (ipAddress.indexOf(",") > 0) {
                     ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
